c910df954070164e9d059a85bf36f4849181f3a3
ruiborda <bordarui@gmail.com>
2022-06-04 14:50:43 -0500

    golan intermedio - interfaces

--- go/intermedio/herencia/main.go
+++ go/intermedio/herencia/main.go
@@ -2,6 +2,10 @@ package main
 
 import "fmt"
 
+type PrintInfo interface {
+	GetInfo() string
+}
+
 type Person struct {
 	Name string
 	Age  uint8
@@ -15,10 +19,19 @@ type FullTimeEmployee struct {
 	Person
 }
 
+func (fullTimeEmployee FullTimeEmployee) GetInfo() string {
+	return fmt.Sprintf("Name: %s, Age: %d, Id: %d", fullTimeEmployee.Name, fullTimeEmployee.Age, fullTimeEmployee.Id)
+}
+
+func GetInfo(employee PrintInfo) string {
+	return employee.GetInfo()
+}
+
 func main() {
 	employee := FullTimeEmployee{}
 	employee.Name = "John"
 	employee.Age = 30
 	employee.Id = 1
 	fmt.Printf("%v\n", employee)
+	fmt.Printf("%v\n", GetInfo(employee))
 }
